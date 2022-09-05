28d67c92fd90ba20b883c44e1d0c8fa03ba91962
jcbarcelosrj <jcbarcelosrj@gmail.com>
2022-09-05 18:12:56 -0300

    feat: add config mongoose and JogodorSchema

--- api-smartranking/src/app.module.ts
+++ api-smartranking/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+require('dotenv').config();
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    MongooseModule.forRoot(`'${process.env.MONGODB}'`, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- api-smartranking/src/jogadores/interfaces/jogadores.interface.ts
+++ api-smartranking/src/jogadores/interfaces/jogadores.interface.ts
@@ -1,15 +1,19 @@
-export interface Jogadores {
+import {Document} from 'mongoose'
+
+export interface Jogadores extends Document{
   readonly _id: string;
   readonly telefoneCelular: string;
   readonly email: string;
   name: string;
   urlFotoJogador: string;
-  desempenho: Desempenho;
+  ranking: string;
+  posicaoRanking: Number;
+  // desempenho: Desempenho;
 }
 
-interface Desempenho {
-  categoria: string;
-  vitorias: number;
-  derrotas: number;
-  pontos: number;
-}
+// interface Desempenho {
+//   categoria: string;
+//   vitorias: number;
+//   derrotas: number;
+//   pontos: number;
+// }
